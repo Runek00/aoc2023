9688df0a3d51427601e0d076c46bd8ce2d82adcf
Runek00 <runek00@gmail.com>
2023-12-02 13:16:42 +0100

    Day 2

---
+++ src/main/java/org/example/Day2.java
@@ -0,0 +1,61 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Day2 {
+
+    record Game(Long id, List<Map<String, Long>> rounds) {
+    }
+
+    static String aoc2(Stream<String> input) {
+        final Map<String, Long> loaded = Map.of("red", 12L, "green", 13L, "blue", 14L);
+        return input
+                .map(Day2::lineToGame)
+                .filter(game -> validGame(loaded, game))
+                .map(Game::id)
+                .reduce(0L, Long::sum)
+                .toString();
+    }
+
+    static String aoc2a(Stream<String> input) {
+        return input
+                .map(Day2::lineToGame)
+                .map(Day2::toSetPower)
+                .reduce(0L, Long::sum)
+                .toString();
+    }
+
+    private static Long toSetPower(Game game) {
+        Map<String, Long> maxMap = game.rounds()
+                .stream()
+                .flatMap(round -> round.entrySet().stream())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (a, b) -> a > b ? a : b));
+        return maxMap.values().stream().reduce(1L, (a, b) -> a * b);
+    }
+
+    private static Game lineToGame(String line) {
+        String[] lsplit = line.split(": ");
+        long id = Long.parseLong(lsplit[0].split(" ")[1]);
+        List<Map<String, Long>> rounds = Arrays.stream(lsplit[1].split("; "))
+                .map(roundString -> Arrays.stream(roundString.split(", "))
+                        .map(cubeString -> cubeString.split(" "))
+                        .collect(Collectors.toMap(csplit -> csplit[1], csplit -> Long.parseLong(csplit[0]), (a, b) -> b)))
+                .toList();
+        return new Game(id, rounds);
+    }
+
+    private static boolean validGame(Map<String, Long> loaded, Game game) {
+        for (Map<String, Long> r : game.rounds()) {
+            for (String k : r.keySet()) {
+                if (r.get(k) > loaded.getOrDefault(k, 0L)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,20 +8,23 @@ import java.util.stream.Stream;
 
 import static org.example.Day1.aoc1;
 import static org.example.Day1.aoc1a;
+import static org.example.Day2.aoc2;
+import static org.example.Day2.aoc2a;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         System.out.println(aoc1(Objects.requireNonNull(fileReader(input1))));
         System.out.println(aoc1a(Objects.requireNonNull(fileReader(input1))));
+        System.out.println(aoc2(Objects.requireNonNull(fileReader(input2))));
+        System.out.println(aoc2a(Objects.requireNonNull(fileReader(input2))));
     }
 
     private static final String test = "src/main/java/org/example/inputs/testInput";
     private static final String input1 = "src/main/java/org/example/inputs/day1Input";
+    private static final String input2 = "src/main/java/org/example/inputs/day2input";
 
     static Stream<String> fileReader(String filePath) {
-        try {
+       try {
             return Files.lines(Path.of(filePath));
         } catch (IOException e) {
             return null;
