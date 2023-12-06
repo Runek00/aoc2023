6dd2e1c6e718c318414fb35710d6b59eee8deb94
Runek00 <runek00@gmail.com>
2023-12-07 00:59:27 +0100

    Some optimization attempts, that didn't change anything

--- src/main/java/org/example/Day5.java
+++ src/main/java/org/example/Day5.java
@@ -1,7 +1,7 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 import java.util.stream.Stream;
@@ -14,19 +14,55 @@ public class Day5 {
         return getMinResult(splinput, seedStream);
     }
 
+    record Range(long start, long end) {
+    }
+
     static long aoc5a(String input) {
         String[] splinput = input.split("\r\n\r\n");
         String[] seedNums = splinput[0].split(": ")[1].split(" ");
         long cand = Long.MAX_VALUE;
-        for (int i = 0; i < seedNums.length; i+=2) {
-            Stream<Long> seedStream = LongStream.range(Long.parseLong(seedNums[i]), Long.parseLong(seedNums[i]) + Long.parseLong(seedNums[i+1])).boxed().parallel();
+        List<Range> ranges = new ArrayList<>();
+        for (int i = 0; i < seedNums.length; i += 2) {
+            ranges.add(new Range(Long.parseLong(seedNums[i]), Long.parseLong(seedNums[i]) + Long.parseLong(seedNums[i + 1])));
+        }
+        ranges = normalizeRanges(ranges);
+        for (Range range : ranges) {
+            Stream<Long> seedStream = LongStream.range(range.start(), range.end()).boxed().parallel();
             cand = Math.min(cand, getMinResult(splinput, seedStream));
         }
         return cand;
     }
 
+    private static List<Range> normalizeRanges(List<Range> ranges) {
+        TreeMap<Long, Range> byStarts = new TreeMap<>(ranges.stream().collect(Collectors.toMap(Range::start, range -> range)));
+        Range firstRange = byStarts.pollFirstEntry().getValue();
+        List<Range> output = new ArrayList<>();
+        long start = firstRange.start();
+        long end = firstRange.end();
+        while (!byStarts.isEmpty()) {
+            Range newRange = byStarts.pollFirstEntry().getValue();
+            if (newRange.start() < end) {
+                end = Math.max(end, newRange.end());
+            } else {
+                output.add(new Range(start, end));
+                start = newRange.start();
+                end = newRange.end();
+            }
+        }
+        output.add(new Range(start, end));
+        return output;
+    }
+
     private static long getMinResult(String[] splinput, Stream<Long> seedStream) {
         List<RangesMapper> transformers = getTransformers(splinput);
+        Range minmax = transformers.stream()
+                .collect(Collectors.teeing(
+                        Collectors.minBy(Comparator.comparing(RangesMapper::minVal)),
+                        Collectors.maxBy(Comparator.comparing(RangesMapper::maxVal)),
+                        (min, max) -> min.isPresent() && max.isPresent() ? new Range(min.get().minVal(), max.get().maxVal()) : null));
+        if (minmax != null) {
+            seedStream = seedStream.filter(l -> l > minmax.start()).filter(l -> l < minmax.end());
+        }
         for (RangesMapper transformer : transformers) {
             seedStream = seedStream.map(transformer::map);
         }
@@ -52,6 +88,14 @@ public class Day5 {
                 }
             }
             return input;
+        }
+
+        long minVal() {
+            return ranges.stream().mapToLong(range -> Math.max(range.targetStart(), range.sourceStart())).min().orElse(Long.MAX_VALUE);
+        }
+
+        long maxVal() {
+            return ranges.stream().mapToLong(range -> Math.max(range.targetStart(), range.sourceStart()) + range.length()).max().orElse(Long.MIN_VALUE);
         }
     }
 
