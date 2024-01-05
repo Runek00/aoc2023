5187f97b40cdf4f4804b62f84af6d9d46546053c
Runek00 <runek00@gmail.com>
2024-01-05 22:43:34 +0100

    Day 23 part 2 attempt

--- src/main/java/org/example/Day22.java
+++ src/main/java/org/example/Day22.java
@@ -35,18 +35,100 @@ public class Day22 {
             return Math.max(start().z(), end().z());
         }
     }
+
     static int[][] topsMap = new int[10][10];
     static Map<Integer, Set<Brick>> levelBricks = new HashMap<>();
+
+    static Map<Brick, Set<Brick>> higherBrickMap = new HashMap<>();
 
     public static long aoc22(Stream<String> input) {
         String[][][] tower = parseTower(input);
         return countRemovable(tower);
     }
 
+    public static long aoc22a(Stream<String> input) {
+        topsMap = new int[10][10];
+        levelBricks = new HashMap<>();
+        higherBrickMap = new HashMap<>();
+        record LevelBrick(Integer k, Brick brick) {
+        }
+        String[][][] tower = parseTower(input);
+        Set<Brick> fallCausing = getNonRemovable(tower);
+        long count = 0L;
+        Set<Brick> usedBrick = new HashSet<>();
+        for (int i = 0; i < 400; i++) {
+            int i1 = 399 - i;
+            Integer integer = i1;
+            for (Brick brick : levelBricks.getOrDefault(integer, new HashSet<>())) {
+                LevelBrick levelBrick = new LevelBrick(integer, brick);
+                if (fallCausing.contains(levelBrick.brick()) && !usedBrick.contains(levelBrick.brick())) {
+                    usedBrick.add(levelBrick.brick());
+                    Set<Brick> higherBricksAsBricks = getHigherBricksAsBricks(levelBrick.brick(), levelBrick.k(), tower);
+                    count+=higherBricksAsBricks.size();
+                }
+            }
+        }
+        return count;
+    }
+
+    private static Set<Brick> getHigherBricksAsBricks(Brick brick, int k, String[][][] tower) {
+        if (higherBrickMap.containsKey(brick)) {
+            return higherBrickMap.get(brick);
+        }
+        Set<Brick> output = new HashSet<>();
+        for (int i = brick.lowerX(); i <= brick.higherX(); i++) {
+            for (int j = brick.lowerY(); j <= brick.higherY(); j++) {
+                if (tower[i][j][k + 1] != null) {
+                    String line = tower[i][j][k + 1];
+                    if (line.equals(brick.name())) {
+                        continue;
+                    }
+                    Brick newBrick = new Brick(cubeFromString(line.split("~")[0]), cubeFromString(line.split("~")[1]), line);
+                    output.add(newBrick);
+                    output.addAll(getHigherBricksAsBricks(newBrick, k + 1, tower));
+                }
+            }
+        }
+        higherBrickMap.put(brick, output);
+        return output;
+    }
+
+    private static Set<Brick> getNonRemovable(String[][][] tower) {
+        Set<Brick> nonEmptyCombos = new HashSet<>();
+        for (int k = 0; k < 400; k++) {
+            if (!levelBricks.containsKey(k)) {
+                break;
+            }
+            Set<Brick> bricks = levelBricks.get(k);
+            int finalK = k;
+            Map<Brick, Set<String>> onThemMap = bricks.stream().collect(Collectors.toMap(brick -> brick, brick -> getHigherBricks(brick, finalK, tower)));
+            nonEmptyCombos.addAll(getNonRemovableSet(onThemMap));
+        }
+        return nonEmptyCombos;
+    }
+
+    private static Set<Brick> getNonRemovableSet(Map<Brick, Set<String>> onThemMap) {
+        Set<Brick> output = new HashSet<>();
+        for (Brick b : onThemMap.keySet()) {
+            if(onThemMap.size() == 1 && onThemMap.get(b).size() == 1) {
+                if(onThemMap.get(b).stream().allMatch(n -> n.equals(b.name()))) {
+                    continue;
+                }
+            }
+            Set<String> set1 = new HashSet<>(onThemMap.get(b));
+            Set<String> set2 = onThemMap.entrySet().stream().filter(entry -> entry.getKey() != b).flatMap(entry -> entry.getValue().stream()).collect(Collectors.toSet());
+            set1.removeAll(set2);
+            if (!set1.isEmpty()) {
+                output.add(b);
+            }
+        }
+        return output;
+    }
+
     private static int countRemovable(String[][][] tower) {
         Set<Brick> emptyCombos = new HashSet<>();
-        for(int k = 0; k < 400; k++) {
-            if(!levelBricks.containsKey(k)) {
+        for (int k = 0; k < 400; k++) {
+            if (!levelBricks.containsKey(k)) {
                 break;
             }
             Set<Brick> bricks = levelBricks.get(k);
@@ -59,11 +141,11 @@ public class Day22 {
 
     private static Set<Brick> getRemovableSet(Map<Brick, Set<String>> onThemMap) {
         Set<Brick> output = new HashSet<>();
-        for(Brick b : onThemMap.keySet()) {
+        for (Brick b : onThemMap.keySet()) {
             Set<String> set1 = new HashSet<>(onThemMap.get(b));
             Set<String> set2 = onThemMap.entrySet().stream().filter(entry -> entry.getKey() != b).flatMap(entry -> entry.getValue().stream()).collect(Collectors.toSet());
             set1.removeAll(set2);
-            if(set1.isEmpty()) {
+            if (set1.isEmpty()) {
                 output.add(b);
             }
         }
@@ -72,10 +154,10 @@ public class Day22 {
 
     private static Set<String> getHigherBricks(Brick brick, int k, String[][][] tower) {
         Set<String> output = new HashSet<>();
-        for(int i = brick.lowerX(); i <= brick.higherX(); i++) {
+        for (int i = brick.lowerX(); i <= brick.higherX(); i++) {
             for (int j = brick.lowerY(); j <= brick.higherY(); j++) {
-                if(tower[i][j][k+1] != null) {
-                    output.add(tower[i][j][k+1]);
+                if (tower[i][j][k + 1] != null) {
+                    output.add(tower[i][j][k + 1]);
                 }
             }
         }
